c969be1d42bdeea51ef6a2032aad945e7a6325d0
Allassane <dickoallassane1997@gmail.com>
2023-05-09 15:08:05 +0100

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(name: "Allassane", text:"Hi, everyone!")
+Message.create(name: "Trast00", text:"How are you ?")
+Message.create(name: "Allassane", text:"Good what's about you ?")
+Message.create(name: "Trast00", text:"I'm Great")
+Message.create(name: "Allassane", text:"FBI! Where do you hide the body")
